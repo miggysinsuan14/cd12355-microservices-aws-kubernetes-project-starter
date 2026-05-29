789851331267bdfeae6e800b719de78e30aa7ca2
miggysinsuan14 <jmiguelsinsuan014@gmail.com>
2026-05-29 09:18:04 +0000

    Add Dockerfile, buildspec, and deployment configs

---
+++ create_cluster.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+eksctl create cluster --name my-cluster --region us-east-1 --nodegroup-name my-nodes --node-type t3.small --nodes 1 --nodes-min 1 --nodes-max 2
---
+++ update_eksctl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl --silent --location "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+mv /tmp/eksctl /usr/local/bin
+eksctl version
